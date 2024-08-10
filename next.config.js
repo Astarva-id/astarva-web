9c504b7699d8664cf4748b7d5fdf165ac63404f0
GannaHup <gannahap02@gmail.com>
2024-08-11 00:35:28 +0700

    feat: init setup astarva web

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Additional Next.js configuration options go here
+};
+
+// module.exports = nextConfig;
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
+import { GlobalStyle } from "@/styles/global-styled";
+import { Flex } from "astarva-ui";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Flex>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Flex>
+  )
 }
