eddb97f6923f6dc358b771e780684b1ba28091d5
GannaHup <gannahap02@gmail.com>
2024-08-13 06:31:39 +0700

    fix: page flick

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
+import usePageRender from "@/hooks/usePageRender";
 import { GlobalStyle } from "@/styles/global-styled";
 import { Flex } from "astarva-ui";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { isReadyMounted } = usePageRender()
+
   return (
     <Flex>
       <GlobalStyle />
-      <Component {...pageProps} />
+      {isReadyMounted && (
+        <Component {...pageProps} />
+      )}
     </Flex>
   )
 }
